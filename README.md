7326676fdbb5caf932eb91a3995d01dccd96a5d7
PavelShturm85 <pavel.shturm@gmail.com>
2017-12-01 17:21:24 +0400

    Вывод информации о 20 курсах в xlsx.

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+**The module get courses url from xml, then finds course info  and write course info about 20 courses in courses_info.xmls.**
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+*Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.*
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+# Quickstart
+**Ways to use:**
+- Have to use  module `coursera.py` after `python3`.
+
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python3 coursera.py
+
+```
+courses_info.xmls
+
+Course name:	                                           Grade:	    Language:	   Start date:	   Amount week:
+Gamification	                                           0 stars	  English	    Starts Dec 04	     6
+Dealing With Missing Data	                               3.8 stars	English	    Started Nov 27	   4
+Vital Signs: Understanding What the Body Is Telling Us	 0 stars	  English	    Started Nov 27	   6
+Modern Art & Ideas	                                     4.6 stars	English	    Started Nov 27	   5
+The Evolving Universe	                                   4.6 stars	English	    Starts Dec 18	     5
+The Changing Global Order	                               4.7 stars	English	    Starts Jan 01	     7
+
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,16 +1,60 @@
+import requests
+from lxml import etree
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 
-def get_courses_list():
-    pass
+def get_courses_url_list():
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    response = requests.get(url).content
+    root = etree.fromstring(response)
+    url_list = []
+    for url in root.getchildren():
+        for loc in url.getchildren():
+            url_list.append(loc.text)
+    return url_list[:20]
 
 
-def get_course_info(course_slug):
-    pass
+def get_course_info(courses_url_list):
+    courses_list = []
+    for courses_url in courses_url_list:
+        url = '{}'.format(courses_url)
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, "lxml")
+
+        grade = getattr(soup.find(
+            "div", class_="ratings-text"), 'text', '0 stars')
+        course_name = getattr(soup.find(
+            "h2", class_="course-name-text"), 'text', 'No data')
+        startdate = getattr(soup.find(
+            "div", class_="startdate"), 'text', 'No data')
+        language = soup.find(
+            "div", class_="rc-Language").findAllNext(text=True)[1]
+        amount_week = len(soup.find_all(
+            "div", class_="week-heading"))
+
+        course = [course_name, grade, language, startdate, amount_week]
+        courses_list.append(course)
+    return courses_list
 
 
 def output_courses_info_to_xlsx(filepath):
-    pass
+    dest_filename = 'courses_info.xlsx'
+    wb = Workbook()
+    sheet = wb.active
+    name_column = (
+        'Course name:',
+        'Grade:',
+        'Language:',
+        'Start date:',
+        'Amount week:'
+    )
+    sheet.append(name_column)
+    for row in filepath:
+        sheet.append(row)
+    wb.save(filename=dest_filename)
 
 
 if __name__ == '__main__':
-    pass
+    course_info = get_course_info(get_courses_url_list())
+    output_courses_info_to_xlsx(course_info)
